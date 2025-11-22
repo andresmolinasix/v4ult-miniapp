785b9342c6025134c49126fcdcd13ada16897d57
Leonardo Ramírez <juanlov4321@gmail.com>
2025-11-22 18:01:38 -0300

    feat: enhance Home component with dynamic background and new UI elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 "use client";
+import { BoxesCore } from "@/app/components/ui/background-boxes";
 // import { SparklesCore } from "@/components/ui/sparkles";
 import ConnectButton from "./components/connectButton";
 import { sdk } from "@farcaster/miniapp-sdk";
 import { useEffect, useState } from "react";
+import BalancingForm from "./components/balancingForm";
+import { TextHoverEffect } from "@/components/ui/text-hover-effect";
+import { EncryptedText } from "@/components/ui/encrypted-text";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { DottedGlowBackground } from "@/components/ui/dotted-glow-background";
+import { FlipWords } from "@/components/ui/flip-words";
+import Footer from "./components/footer";
 
 export default function Home() {
 	const [isMiniApp, setIsMiniApp] = useState(false);
@@ -29,17 +37,87 @@ export default function Home() {
 			cancelled = true;
 		};
 	}, []);
-	return (
-		<div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-			{/* <BoxesCore /> */}
-			<div className='flex flex-col items-center gap-4'>
-				<ConnectButton disabled={!isMiniApp} />
-				{!isMiniApp && (
-					<p className='text-sm text-gray-600 dark:text-gray-300'>
-						Abre esta miniapp desde Farcaster para habilitar la conexión a tu wallet.
-					</p>
-				)}
+
+	if (!isMiniApp) {
+		return (
+			<div className='relative overflow-hidden'>
+				<div className='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 overflow-hidden'>
+					<div className='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'></div>
+				</div>
+				<div className='flex min-h-screen items-center justify-center flex-col'>
+					<div className='flex flex-col items-center gap-4'>
+						<div className='h-[15rem] flex items-center justify-center z-10 select-none flex-col'>
+							<DottedGlowBackground
+								className='pointer-events-none mask-radial-to-90% mask-radial-at-center opacity-20 dark:opacity-100'
+								opacity={0.5}
+								gap={10}
+								radius={1.6}
+								colorLightVar='--color-neutral-500'
+								glowColorLightVar='--color-neutral-600'
+								colorDarkVar='--color-neutral-500'
+								glowColorDarkVar='--color-sky-800'
+								backgroundOpacity={0}
+								speedMin={0.3}
+								speedMax={1.6}
+								speedScale={1}
+							/>
+							<TextHoverEffect text='V4ULT' />
+						</div>
+					</div>
+					<div className='text-center max-w-md mx-auto'>
+						<ConnectButton disabled={!isMiniApp} />
+						<p className='text-sm text-gray-600 dark:text-gray-300 mt-4'>
+							Open this MiniApp inside the Farcaster mobile app to connect your wallet.
+						</p>
+					</div>
+				</div>
+				<Footer />
 			</div>
-		</div>
+		);
+	}
+
+	return (
+		<>
+			<div className='relative overflow-hidden'>
+				<div className='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 overflow-hidden'>
+					<div className='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'></div>
+				</div>
+				<div className='flex min-h-screen items-center justify-center '>
+					<div className='flex flex-col items-center gap-4'>
+						<div className='h-[15rem] flex items-center justify-center z-10 select-none flex-col'>
+							<DottedGlowBackground
+								className='pointer-events-none mask-radial-to-90% mask-radial-at-center opacity-20 dark:opacity-100'
+								opacity={0.5}
+								gap={10}
+								radius={1.6}
+								colorLightVar='--color-neutral-500'
+								glowColorLightVar='--color-neutral-600'
+								colorDarkVar='--color-neutral-500'
+								glowColorDarkVar='--color-sky-800'
+								backgroundOpacity={0}
+								speedMin={0.3}
+								speedMax={1.6}
+								speedScale={1}
+							/>
+							<TextHoverEffect text='V4ULT' />
+
+							<div className='text-xl mx-auto font-normal text-neutral-600 dark:text-neutral-400 text-center'>
+								Manage <FlipWords words={["dynamic", "profitable", "low-risk"]} />
+								liquidity with a single deposit
+							</div>
+						</div>
+
+						{/* <ConnectButton disabled={!isMiniApp} />
+						{!isMiniApp && (
+							<p className='text-sm text-gray-600 dark:text-gray-300'>
+								Open this MiniApp inside the Farcaster mobile app to connect your wallet.
+							</p>
+						)} */}
+						<BalancingForm />
+					</div>
+				</div>
+				<Footer />
+			</div>
+		</>
 	);
 }
