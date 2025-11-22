b1b5dfe75b530becfe6626fc73b8864531999d1b
Leonardo Ramírez <juanlov4321@gmail.com>
2025-11-22 18:03:42 -0300

    feat: add Footer component with basic layout and styling

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer className='footer sm:footer-horizontal footer-center text-base-content p-4'>
+			<aside>
+				<p className='italic'>Keep it Simple, Stupid</p>
+				<p>with ❤️ by V4ULT &amp; Team</p>
+			</aside>
+		</footer>
+	);
+};
+export default Footer;
